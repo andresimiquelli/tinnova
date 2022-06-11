1da6d31d84ee97d5f5704df296ab6c092888b93f
João André Simiquelli <andresimiquelli@hotmail.com>
2022-06-11 08:44:30 -0300

    Spring boot start

---
+++ tinnova-veiculos/src/main/java/com/andresimiquelli/hrpayroll/TinnovaVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnovaVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnovaVeiculosApplication.class, args);
+	}
+
+}
---
+++ tinnova-veiculos/src/test/java/com/andresimiquelli/hrpayroll/TinnovaVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinnovaVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
